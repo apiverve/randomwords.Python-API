b1279d7073c673fb89314277832f83b130aed1c8
APIVerve Team <hello@apiverve.com>
2025-03-28 17:45:32 -0500

    Bug Fixes

--- src/apiverve_randomword/apiClient.py
+++ src/apiverve_randomword/apiClient.py
@@ -10,10 +10,10 @@ class RandomwordsAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/randomwords'
+        self.base_url = 'https://api.apiverve.com/v1/randomwords'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
